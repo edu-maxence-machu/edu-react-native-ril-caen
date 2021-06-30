e04e9c04b459b87edf5043b844731157b9bf0cd0
maxencemachu <maxence.machu@akarah.com>
2021-06-30 10:49:08 +0200

    hotfix(import): add Button component

--- pages/Camera.js
+++ pages/Camera.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {View, Text, StyleSheet, Button} from 'react-native';
 import { BarCodeScanner } from 'expo-barcode-scanner';
 
 export default function Camera(){
